ecf6ea8f642778cfcc9f45fc978a7ebd788bcbd6
felipe chaux <felipechauxx@misena.edu.co>
2026-02-06 23:46:38 -0500

    Configure app icons and update .gitignore

--- app/src/main/java/com/chauxdevapps/dadomatch/DadoMatchApplication.kt
+++ app/src/main/java/com/chauxdevapps/dadomatch/DadoMatchApplication.kt
@@ -1,16 +1,11 @@
 package com.chauxdevapps.dadomatch
 
 import android.app.Application
-import com.dadomatch.shared.initKoin
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
+import com.dadomatch.shared.initKoinAndroid
 
 class DadoMatchApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        initKoin {
-            androidContext(this@DadoMatchApplication)
-            androidLogger()
-        }
+        initKoinAndroid(this)
     }
 }
